92efb31b72a011d9f1b14e799ccd580092094294
human2l <human2lkai@gmail.com>
2017-10-24 18:17:11 +1100

    userSignupPage update

---
+++ lms/lmsclient/src/app/auth/SignUpPageCtrl.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+  
+    angular.module('BlurAdmin.auto.signup')
+      .controller('SignUpPageCtrl', SignUpPageCtrl);
+  
+    /** @ngInject */
+    function SignUpPageCtrl($scope, $state) {
+      
+      
+    }
+  
+  })();
